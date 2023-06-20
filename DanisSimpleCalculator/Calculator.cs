0c10df1a4f9b7a48b54c1516ea7c4b93dff1c4b7
Daniel_Fridman <daniel.f@sigasec.com>
2023-06-20 10:24:11 +0300

    .

---
+++ DanisSimpleCalculator/Calculator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DanisSimpleCalculator
+{
+    public class Calculator
+    {
+        public double Add(double a, double b)
+        {
+            return a + b;
+        }
+
+        public string Jsonify(double a)
+        {
+            return "{\"a\": " + a.ToString() + "}");
+        }
+
+        public string ToHtml(double a)
+        {
+            return $"<h5>{a}</h5>";
+        }
+
+        public double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+    }
+}
---
+++ DanisSimpleCalculator/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DanisSimpleCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var c = new Calculator();
+
+            double sum = c.Add(4, 5);
+            string html = c.ToHtml(85);
+            string json = c.Jsonify(9);
+            double mul = c.Multiply(4, 5);
+
+        }
+
+    }
+}
